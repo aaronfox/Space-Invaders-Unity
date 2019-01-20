9c5fe50afc0d0f2933e9aa674c0b48fa8afa84b5
aaronfox <aaron.m.fox3@gmail.com>
2019-01-20 14:11:52 -0500

    added basic enemy pathing system

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move() {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            Debug.Log("In move if statement");
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                Debug.Log("Same position");
+                waypointIndex++;
+            }
+            else {
+                Debug.Log("!!!transform.position == " + transform.position + ", targetPosition == " + targetPosition);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,16 +10,19 @@ public class Player : MonoBehaviour
     [SerializeField] float padding = .05f;
     [SerializeField] GameObject laser;
     [SerializeField] float laserSpeed = 11f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
 
+
+    Coroutine firingCoroutine;
     float xMin;
     float xMax;
     float yMin;
     float yMax;
+    
 
     // Start is called before the first frame update
     void Start() {
         SetUpMoveBoundaries();
-        StartCoroutine(PrintCoroutine());
     }
 
     
@@ -33,11 +36,10 @@ public class Player : MonoBehaviour
     private void Fire()
     {
         if(Input.GetButtonDown("Fire1")) {
-            GameObject firedLaser = Instantiate(
-                laser,
-                transform.position,
-                Quaternion.identity) as GameObject;
-            firedLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if(Input.GetButtonUp("Fire1")) {
+            StopCoroutine(firingCoroutine);
         }
     }
 
@@ -62,11 +64,15 @@ public class Player : MonoBehaviour
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
+    IEnumerator FireContinuously() {
 
-    IEnumerator PrintCoroutine() {
-        Debug.Log("In print coroutine");
-        yield return new WaitForSeconds(3);
-        Debug.Log("Wow those 3 seconds took a long time!!!");
+        while(true) {
+            GameObject firedLaser = Instantiate(
+                laser,
+                transform.position,
+                Quaternion.identity) as GameObject;
+            firedLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
     }
-    
 }
