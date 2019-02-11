a0e0e17ff854c451838df7448d6dc6a5fbfd628a
aaronfox <aaron.m.fox3@gmail.com>
2019-02-10 20:48:58 -0500

    working toward adding score display

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    int score = 0;
+
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        int numberOfGameSessions = FindObjectsOfType<GameSession>().Length;
+        if(numberOfGameSessions > 1)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
